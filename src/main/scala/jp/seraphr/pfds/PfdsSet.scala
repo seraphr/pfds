afc8eb353e92d41158f46aeeb0f4b034a0d03743
seraph <seraph>
2017-07-10 02:07:02 +0900

    課題2-2

--- src/main/scala/jp/seraphr/pfds/PfdsSet.scala
+++ src/main/scala/jp/seraphr/pfds/PfdsSet.scala
@@ -19,18 +19,31 @@ trait TreeSet extends PfdsSet {
   type Set = Tree
 
   override val empty: Set = E
-  override val member: (Elem, Set) => Boolean = {
-    case (_, E) => false
+
+
+  /** 課題2-2  */
+  private def memberImpl(aElem: Elem, aSet: Set, aCandidate: Option[Elem]): Boolean = (aElem, aSet) match {
+    case (x, E) => aCandidate.fold(false)(_.compare(x) == 0)
     case (x, T(a, y, b)) =>
-      if(x < y) member(x, a)
-      else if(y < x) member(x, b)
-      else true
+      if(x <= y) memberImpl(x, a, Some(y))
+      else memberImpl(x, b, aCandidate)
   }
+
+  /** 課題2-2  */
+  override val member: (Elem, Set) => Boolean = memberImpl(_, _, None)
+
+  //  override val member: (Elem, Set) => Boolean = {
+  //    case (_, E) => false
+  //    case (x, T(a, y, b)) =>
+  //      if(x < y) member(x, a)
+  //      else if(y < x) member(x, b)
+  //      else true
+  //  }
   override val insert: (Elem, Set) => Set = {
     case (x, E) => T(E, x, E)
     case (x, s@T(a, y, b)) =>
-      if(x < y) T(insert(x, a), y, b)
-      else if(y < x) T(a, y, insert(x, b))
+      if (x < y) T(insert(x, a), y, b)
+      else if (y < x) T(a, y, insert(x, b))
       else s
   }
 }
